d155bba7b429880281825b1d7c29cfb75dd63253
Jaifran Hernandez <jaifranjose@gmail.com>
2025-01-10 15:44:00 -0400

    Feat: Parent root layout file

---
+++ frontend/src/layout/RootLayout.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="min-h-screen bg-background">
+      {children}
+    </div>
+  )
+}
